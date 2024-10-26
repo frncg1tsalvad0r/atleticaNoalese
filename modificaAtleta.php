96e49486ba696c4b9892bb68474f5a230094ff65
Franco Salvador <frnc.salvador@libero.it>
2024-10-26 12:46:41 +0200

    corretto modificaAtleta

--- modificaAtleta.php
+++ modificaAtleta.php
@@ -7,52 +7,54 @@
 </head>
 <body>
     <?php
+        $conn = mysqli_connect("127.0.0.1", "root", "", "atleticanoalese");
+
+
         if(isset($_POST['conferma'])) {
-            $sql = "INSERT INTO atleti (
-                        nome,
-                        cognome,
-                        numeroTessera,
-                        sesso,
-                        categoria,
-                        dataNascita,
-                        registrato
-                    )  
-                    VALUES (
-                        '$_POST[nome]',
-                        '$_POST[cognome]',
-                        $_POST[numeroTessera],
-                        '$_POST[sesso]',
-                        '$_POST[categoria]',
-                        '$_POST[dataNascita]',
-                        $_POST[registrato]
-                    )";
-                $conn = mysqli_connect("127.0.0.1", "root", "", "atleticanoalese");
+            $sql = "UPDATE atleti 
+                        SET 
+                        nome = '$_POST[nome]',
+                        cognome = '$_POST[cognome]',
+                        numeroTessera = $_POST[numeroTessera],
+                        sesso = '$_POST[sesso]',
+                        categoria = '$_POST[categoria]',
+                        dataNascita = '$_POST[dataNascita]',
+                        registrato = $_POST[registrato] WHERE ID = $_POST[ID]";
+                // print_r($sql); die();
                 mysqli_query($conn, $sql);
                 header("Location: visualizzaAtleti.php");
+                die();
         }
+
+        $sql = "SELECT * FROM atleti WHERE ID = $_POST[selezione]";
+        $atleti = mysqli_query($conn, $sql); 
+        $atleta = mysqli_fetch_assoc($atleti);
+        //print_r($atleta); die();
+
     ?>
     <h1>Modifica Atleta</h1>
-    <form action="inserisciAtleta.php" method="POST">
-        <!-- ID: <input type="number" name="ID"><br><br> -->
-        NOME: <input type="text" name="nome"><br><br>
-        COGNOME: <input type="text" name="cognome"><br><br>
-        NUMERO TESSERA: <input type="number" name="numeroTessera"><br><br>
+    <form action="modificaAtleta.php" method="POST">
+        ID: <input type="text" name="ID" readonly value = "<?php echo $atleta['ID']; ?>"><br><br>
+        NOME: <input type="text" name="nome" value = "<?php echo $atleta['nome']; ?>"><br><br>
+        COGNOME: <input type="text" name="cognome" value = "<?php echo $atleta['cognome']; ?>"><br><br>
+        NUMERO TESSERA: <input type="number" name="numeroTessera" value = "<?php echo $atleta['numeroTessera']; ?>"><br><br>
         SESSO: 
-            M:<input type="radio" name="sesso" value='M'>
-            F:<input type="radio" name="sesso" value='M'><br><br>
+            M:<input type="radio" name="sesso" value='M' <?php if($atleta['sesso'] == 'M') echo 'checked'; ?>>
+            F:<input type="radio" name="sesso" value='F' <?php if($atleta['sesso'] == 'F') echo 'checked'; ?>><br><br>
         CATEGORIA: 
         <select name="categoria">
-            <option value="ragazzo">ragazzo</option>
-            <option value="cadetto">cadetto</option>
-            <option value="allievo">allievo</option>
-            <option value="juniores">juniores</option>
-            <option value="promessa">promessa</option>
-            <option value="senior">senior</option>
+            <option value="ragazzo" <?php if($atleta['categoria'] == 'ragazzo') echo 'selected'; ?> >ragazzo</option>
+            <option value="cadetto" <?php if($atleta['categoria'] == 'cadetto') echo 'selected'; ?> >cadetto</option>
+            <option value="allievo" <?php if($atleta['categoria'] == 'allievo') echo 'selected'; ?>>allievo</option>
+            <option value="juniores" <?php if($atleta['categoria'] == 'juniores') echo 'selected'; ?>>juniores</option>
+            <option value="promessa" <?php if($atleta['categoria'] == 'promessa') echo 'selected'; ?>>promessa</option>
+            <option value="senior" <?php if($atleta['categoria'] == 'senior') echo 'selected'; ?>>senior</option>
             
         </select><br><br>
-        DATA NASCITA: <input type="date" name="dataNascita"><br><br>
+        DATA NASCITA: <input type="date" name="dataNascita" value = "<?php echo $atleta['dataNascita']; ?>"><br><br>
+        <!-- REGISTRATO:<input type="checkbox" name="registrato" value = "1" checked><br><br> -->
         <input type="hidden" name="registrato" value="0">
-        REGISTRATO: <input type="checkbox" name="registrato" value="1"><br><br>
+        REGISTRATO: <input type="checkbox" name="registrato" value = "1" <?php if($atleta['registrato'] == 1) echo 'checked'; ?> ><br><br>
         <input type="submit" name="conferma" value="Conferma"><br><br>
     </form>
 
